0f1e222a96bec0f1113b67c039f5b8dcc1ab0e0b
bryan <bryan.guner@gmail.com>
2021-02-19 03:48:43 -0500

    logo

--- RocktheCradle_RTR_Clone-main/frontend/src/components/Navigation/index.js
+++ RocktheCradle_RTR_Clone-main/frontend/src/components/Navigation/index.js
@@ -42,7 +42,7 @@ function Navigation({ isLoaded }) {
         <nav className="fixed opacity-100 h-20 z-99 bg-white shadow-md grid grid-cols-10 w-screen">
             <div className="fixed opacity-100 h-20 z-99 shadow-md grid grid-cols-10 w-screen">
                 <div className="col-start-1 col-end-2">
-                    <NavLink exact to="/"><img src="https://rtcmediabucket.s3.amazonaws.com/assets/rtc-transparent.png" alt="logo" className="h-20 ml-20 transition duration-100 hover:shadow ease" /></NavLink>
+                    <NavLink exact to="/"><img src="https://i.imgur.com/pZpLFUO.jpg" alt="logo" className="h-20 ml-20 transition duration-100 hover:shadow ease" /></NavLink>
                 </div>
 
                 <div className="col-start-2 col-end-4">
@@ -96,7 +96,7 @@ function Navigation({ isLoaded }) {
     // <nav className="sticky bg-grey-600 shadow-md h-20 z-50 w-full px-5 py-2 flex justify-between items-center cursor-pointer">
     //             <div className="flex">
 
-    //                 <NavLink exact to="/" className="h-20 w-20 self-center items-center"><img src="https://rtcmediabucket.s3.amazonaws.com/assets/rtc-transparent.png" alt="logo" /></NavLink>
+    //                 <NavLink exact to="/" className="h-20 w-20 self-center items-center"><img src="https://i.imgur.com/pZpLFUO.jpg" alt="logo" /></NavLink>
     //             </div>
 
     //             <div className="flex w-full m-auto px-20 items-center content-center">
